c02125cec1fb687e51c6162f3d34d2af4bbbfd43
Mohamed Luay <mohamed.luay89@gmail.com>
2019-10-16 16:03:39 +0200

    [A]- gflow_graphical_selector.sh

--- test.sh
+++ test.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
+set -o nounset
 
-. gflow_logger
+#### Import Gflow Modules
+. "./modules/gflow_logger.sh"
+. "./modules/gflow_constants.sh"
+. "./modules/gflow_graphical_selector.sh"
 
-gflow_logger_error "Write Hello"
+gflow_log_error "Error Message $gflow_folder_name"
+gflow_log_warning "Warning Message"
+gflow_log_successful "Successful Message"
+gflow_log_highlight "Higligh Message"
+
+options=("1- try again ⚙️" "2- Abort Commit 🛑🚦")
+    select_option "${options[@]}"    
+    choice=$?
+    case $choice in
+        0) echo "hi";;
+        1) echo "hi ";;
+    esac
