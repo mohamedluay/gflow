5cbf9d36a76501465684b66f3888b95d2409354a
Mohamed Luay <mohamed.luay89@gmail.com>
2019-10-16 15:48:28 +0200

    [A]- gflow_constants.sh

--- gflow.sh
+++ gflow.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -o nounset
 
+#### Import Gflow Modules
 . "./modules/gflow_logger.sh"
+. "./modules/gflow_constants.sh"
 
-gflow_log_error "Error Message"
+gflow_log_error "Error Message $gflow_folder_name"
 gflow_log_warning "Warning Message"
 gflow_log_successful "Successful Message"
 gflow_log_highlight "Higligh Message"
---
+++ modules/gflow_constants.sh
@@ -0,0 +1,4 @@
+gflow_folder_name="./.gflow"
+config_file="$gflow_folder_name/config.json"
+temp_changelog_file="$gflow_folder_name/temp_changelog.md"
+gflow_version="1.1.0"
